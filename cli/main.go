92cad1152480f2666bdaaa35ee2ce54994baa9c3
Louis <tagnacht2@gmail.com>
2026-03-20 21:26:27 +0100

    initial commit

---
+++ cli/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	ait "github.com/Throvn/appimagetool.go"
+)
+
+const (
+	ARCH_x86_64  = "x86_64"
+	ARCH_aarch64 = "aarch64"
+	ARCH_i686    = "i686"
+	ARCH_armhf   = "armhf"
+)
+
+func main() {
+	location, err := os.Getwd()
+	ait.Check(err)
+	outFileName := filepath.Join(location, "test_fs.squashfs")
+
+	os.Remove(outFileName)
+	ait.CreateSquashFSFromFolder(filepath.Join(location, "test_fs"), outFileName)
+	fileName := ait.DownloadAppImageEngine(ARCH_x86_64)
+	ait.AppendToFile(outFileName, fileName)
+	hash := ait.CalculateMD5(fileName)
+	ait.UpdateMD5(fileName, hash)
+	fmt.Println(hex.EncodeToString(hash))
+}
---
+++ fetch_runtime.go
@@ -0,0 +1,48 @@
+package appimagetoolgo
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const DOWNLOAD_URL = "https://github.com/AppImage/type2-runtime/releases/download/continuous"
+
+func downloadFromUrl(url string) string {
+	tokens := strings.Split(url, "/")
+	fileName := tokens[len(tokens)-1]
+	fmt.Println("Downloading", url, "to", fileName)
+
+	output, err := os.Create(fileName)
+	if os.IsExist(err) {
+		// File already exists, skip downloading.
+		return fileName
+	} else if err != nil {
+		fmt.Println("Error while creating", fileName, "-", err)
+		return ""
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return ""
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return ""
+	}
+
+	fmt.Println(n, "bytes downloaded.")
+	return fileName
+}
+
+func DownloadAppImageEngine(arch string) string {
+	dlLocation := downloadFromUrl(DOWNLOAD_URL + "/runtime-" + arch)
+	return dlLocation
+}
